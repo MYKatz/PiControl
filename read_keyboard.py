cfd67511b04546d58e5d531dca17910ad8dac3e1
Matt Katz <mykatz01@gmail.com>
2020-02-06 20:07:54 -0800

    keyboard update

--- read_keyboard.py
+++ read_keyboard.py
@@ -7,18 +7,26 @@ import requests
 #Environment vars
 endpoint = "http://localhost:3000" #where picontrol server is hosted
 
+log = open("MyFile.txt","a") 
+
 pi_address = str(uuid.getnode()) #48-bit integer string, Mac address
 
+'''
 register = requests.post(endpoint + "/api/pis/register/" + pi_address)
 if register.status_code == 200:
     print("pi is registered :)")
 else:
     raise Exception("failed to register pi -- please restart")
+'''
 
 while True: #always be readin'
 
     nfc_uid = input("card uid: ")
+
+    log.write(nfc_uid)
+
     # Try again if no card is available.
+    '''
     if nfc_uid is not None:
 
         sendReq = requests.post("%s/api/scanticket/%s?serial=%s" % (endpoint, pi_address, nfc_uid))
@@ -26,3 +34,4 @@ while True: #always be readin'
             print("something went wrong")
 
         #TODO: do something with the cube's colors -- tooodooo
+    '''

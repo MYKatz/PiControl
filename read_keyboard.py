b468a2cd25b4b45f29cde3b45d377cd61066dbd7
Matt Katz <mykatz01@gmail.com>
2020-02-09 00:23:38 -0800

    update keyboard reading code

--- read_keyboard.py
+++ read_keyboard.py
@@ -6,17 +6,13 @@ import serial
 
 import sys
 
-sys.stdin = open('/dev/input/event0','rb')
-#import pickledb
+reader = open('/dev/input/event0','rb')
 
-#Environment vars
 endpoint = "http://localhost:3000" #where picontrol server is hosted
 
 log = open("MyFile.txt","a") 
 
 pi_address = str(uuid.getnode()) #48-bit integer string, Mac address
-
-#ser = serial.Serial('/dev/input/event0',19200, timeout = 5)
 
 '''
 register = requests.post(endpoint + "/api/pis/register/" + pi_address)
@@ -26,13 +22,28 @@ else:
     raise Exception("failed to register pi -- please restart")
 '''
 
+code = ""
+
 while True: #always be readin'
 
-    nfc_uid = input("card uid: ")
-    print("----uid-----")
-    print(nfc_uid.decode("ascii"))
-    print("---end uid ----")
-    log.write(nfc_uid)
+    buffer = fp.read(1)
+    for c in buffer:
+        if c > 0:
+            print(c)
+            if c == 30 : code = code + '1'
+            elif c == 31 : code = code + '2'
+            elif c == 32 : code = code + '3'
+            elif c == 33 : code = code + '4'
+            elif c == 34 : code = code + '5'
+            elif c == 35 : code = code + '6'
+            elif c == 36 : code = code + '7'
+            elif c == 37 : code = code + '8'
+            elif c == 38 : code = code + '9'
+            elif c == 39 : code = code + '0'
+            elif c == 40 :
+                print('code:' + code) # debug
+
+                code = ''
 
     # Try again if no card is available.
     '''
